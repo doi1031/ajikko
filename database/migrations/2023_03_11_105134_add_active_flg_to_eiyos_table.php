de1e0ab67b02943a70bd79419c3290f2aadbce8f
doi1031 <118028791+doi1031@users.noreply.github.com>
2023-03-11 11:07:52 +0900

    食品フラグ立てするやつ active_flg 追加して DBでフラグ立てしたやつからフォームの選択肢もってくるようにした

---
+++ database/migrations/2023_03_11_105134_add_active_flg_to_eiyos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('eiyos', function (Blueprint $table) {
+            $table->boolean('active_flg',)
+                ->default(false)
+                ->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('eiyos', function (Blueprint $table) {
+            //
+        });
+    }
+};
