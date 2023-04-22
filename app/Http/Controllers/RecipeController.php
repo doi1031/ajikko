d71084272684cb5f65ffc858f03ea08f7fa8091f
doi1031 <118028791+doi1031@users.noreply.github.com>
2023-04-22 10:35:54 +0900

    ソート機能追加

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -8,16 +8,35 @@ use App\Models\AjikkoRecipe;
 
 class RecipeController extends Controller
 {
-    public function index()
-    {
-        $recipes = AjikkoRecipe::all()->map(function(AjikkoRecipe $ajikkoRecipe){
-            return $ajikkoRecipe->toDomain();
-        });
-        $params = [
-            'recipes' => $recipes,
-        ];
-        return view('recipes/index', $params);
+    public function index(Request $request)
+{
+    $sort = $request->input('sort');
+    $query = AjikkoRecipe::query();
+
+    switch ($sort) {
+        case 'low_fat':
+            $query->orderBy('fat', 'asc');
+            break;
+        case 'high_protein':
+            $query->orderBy('prot', 'desc');
+            break;
+        case 'newest':
+            $query->orderBy('created_at', 'desc');
+            break;
     }
+
+    $ajikkoRecipes = $query->get();
+    $recipes = $ajikkoRecipes->map(function (AjikkoRecipe $ajikkoRecipe) {
+        return $ajikkoRecipe->toDomain();
+    });
+
+    $params = [
+        'recipes' => $recipes,
+    ];
+
+    return view('recipes/index', $params);
+}
+
 
     public function show($id)
     {
